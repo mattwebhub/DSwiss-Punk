0e398f6be136262ba25bf42fdebd69c252b85991
Matheus <matheus_paranhos@outlook.com.br>
2023-09-20 12:44:42 +0100

    test: Update Tests

--- DSwiss-Punk/Core/Services/ProductService.cs
+++ DSwiss-Punk/Core/Services/ProductService.cs
@@ -1,40 +1,37 @@
 using System.Diagnostics;
 using System.Net.Http.Json;
-using System.Text.Json;
 using DSwiss_Punk.Core.Models;
 
 namespace DSwiss_Punk.Core.Services
 {
     public class ProductService
     {
-        private HttpClient httpClient;
-        private List<Product> productList;
-        private string apiUrl = "https://api.punkapi.com/v2/beers?";
+        private HttpClient _httpClient;
+        private List<Product> _productList;
+        private const string ApiUrl = "https://api.punkapi.com/v2/beers?";
 
         public ProductService()
         {
-            this.httpClient = new HttpClient();
+            // Note: We would want to modify this to accept this instance on the constructor, so that it can be more testable
+            // For time constraints we will leave it as is.
+            this._httpClient = new HttpClient();
         }
 
         public async Task<List<Product>> GetProductsAsync(int pageNumber, int pageSize)
         {
-            if (productList?.Count > 0)
-                return productList;
-            if (apiUrl == null)
-            {
-                return new List<Product>();
-            }
+            if (_productList?.Count > 0)
+                return _productList;
 
-            var response = await httpClient.GetAsync($"{apiUrl}/page={pageNumber}&per_page={pageSize}");
+            var response = await _httpClient.GetAsync($"{ApiUrl}/page={pageNumber}&per_page={pageSize}");
 
             if (response.IsSuccessStatusCode)
             {
-                productList = await response.Content.ReadFromJsonAsync<List<Product>>();
+                _productList = await response.Content.ReadFromJsonAsync<List<Product>>();
             }
 
-            Debug.WriteLine(productList);
+            Debug.WriteLine(_productList);
 
-            return productList;
+            return _productList;
         }
     }
 }
--- DSwiss-Punk_Tests/Services/ProductServiceTest.cs
+++ DSwiss-Punk_Tests/Services/ProductServiceTest.cs
@@ -1,12 +1,9 @@
 ﻿using FluentAssertions;
-using NSubstitute;
 using DSwiss_Punk.Core.Models;
 using DSwiss_Punk.Core.Services;
-using System.Net.Http;
 using System.Net;
-using System.Text;
 using Newtonsoft.Json;
-using Xunit;
+
 
 namespace DSwiss_Punk_Tests.Services
 {
@@ -22,21 +19,43 @@ namespace DSwiss_Punk_Tests.Services
                 new Product { Id = 2, Name = "Trashy Blonde", Tagline = "You Know You Shouldn't", First_brewed = "04/2008", Description = "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.", Image_url = "https://images.punkapi.com/v2/2.png" },
             };
 
-            var httpClient = Substitute.For<HttpClient>();
-            string expectedUri = "https://api.punkapi.com/v2/beers?page=1&per_page=2";
-            httpClient.GetAsync(expectedUri)
-                      .Returns(new HttpResponseMessage(HttpStatusCode.OK)
-                      {
-                          Content = new StringContent(JsonConvert.SerializeObject(expectedProducts), Encoding.UTF8, "application/json")
-                      });
+            var httpMessageHandler = new MockHttpMessageHandler(JsonConvert.SerializeObject(expectedProducts), HttpStatusCode.OK);
+            var httpClient = new HttpClient(httpMessageHandler);
 
             var productService = new ProductService();
-
+            
             // Act
             var products = await productService.GetProductsAsync(1, 2);
 
             // Assert
-            products.Should().BeEquivalentTo(expectedProducts);
+            products.Should().BeEquivalentTo(expectedProducts, options => options
+                .Including(p => p.Id)
+                .Including(p => p.Name)
+                .Including(p => p.Tagline)
+                .Including(p => p.First_brewed)
+                .Including(p => p.Description)
+                .Including(p => p.Image_url));
+        }
+    }
+
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly string _response;
+        private readonly HttpStatusCode _statusCode;
+
+        public MockHttpMessageHandler(string response, HttpStatusCode statusCode)
+        {
+            _response = response;
+            _statusCode = statusCode;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = _statusCode,
+                Content = new StringContent(_response)
+            };
         }
     }
 }
